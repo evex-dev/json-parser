8d376fd85e8847b55133139d89d969008851c615
Ame_x Edam <edame8080@gmail.com>
2024-06-18 01:49:23 +0000

    fix

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,5 @@ type JSONPrimitiveTypes = string | number | boolean | null;
 type JSONValue = JSONPrimitiveTypes | JSONObject | JSONArray;
 type JSONObject = { [key: string]: JSONValue };
 type JSONArray = JSONValue[];
+
+export { JSONPrimitiveTypes, JSONValue, JSONObject, JSONArray };
