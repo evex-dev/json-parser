0e3c43edad090ab628ca156b285dc1e12e97509d
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-10-11 15:05:03 +0900

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,10 @@
 import type { JSONValue } from "./types.ts";
 
+if (typeof window === "undefined") {
+    throw new Error("Do not use this In Server-Side")
+}
+
 export function JSONParser(string: string): JSONValue {
     // Temp
-    return JSON.parse(string);
+    return new Function(`return (${string})`);
 }
